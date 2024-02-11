9bf5a05d151f8a824172482808d5fba9d98daf0e
zholudev111 <zholudev111@gmail.com>
2024-02-11 13:07:24 +0200

    added verifing for google | fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <meta name='google-site-verification' content='WQCEkvzvRCZ1IFQfHuvJ0pkyF5ANkeJViDJJxepIbrU' />
       <body className={`font-sans ${inter.variable}`}>
         <TRPCReactProvider>
           <main>{children}</main>
