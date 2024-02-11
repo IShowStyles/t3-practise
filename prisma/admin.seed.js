dd97016c221b3f59eaf558bcc372d760d189aa4c
zholudev111 <zholudev111@gmail.com>
2024-02-11 17:47:08 +0200

    change extension seedsfix

--- prisma/admin.seed.js
+++ prisma/admin.seed.js
@@ -5,8 +5,13 @@ import * as process from 'process';
 const prisma = new PrismaClient();
 
 async function main() {
-  const adminEmail = process.env.ADMIN_EMAIL;
-  const adminPassword = process.env.ADMIN_PASSWORD;
+  const adminEmail = process.env.ADMIN_EMAIL?.toString(); // Optional chaining
+  const adminPassword = process.env.ADMIN_PASSWORD?.toString(); // Optional chaining
+
+  if (!adminEmail || !adminPassword) {
+    throw new Error('Admin email or password is not provided.');
+  }
+
   const hashedPassword = await bcrypt.hash(adminPassword, 10); // 10 is the number of salt rounds
   const admin = await prisma.user.upsert({
     where: { email: adminEmail },
