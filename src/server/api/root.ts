d268acc14e0b16dd4b532eb9f494381f2951ea19
zholudev111 <zholudev111@gmail.com>
2024-02-11 18:41:07 +0200

    change extension seedsfix2

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from '~/server/api/trpc';
 import { pizzaRouter } from '~/server/api/routers/pizza';
 import { authRouter } from '~/server/api/routers/register';
+import { usersRouter } from '~/server/api/routers/users';
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { authRouter } from '~/server/api/routers/register';
 export const appRouter = createTRPCRouter({
   pizza: pizzaRouter,
   auth: authRouter,
+  users: usersRouter,
 });
 
 // export type definition of API
