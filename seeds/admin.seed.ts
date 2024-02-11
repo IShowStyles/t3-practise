370bde68f6407fca66ac354bb3f8dac5ed531dbe
zholudev111 <zholudev111@gmail.com>
2024-02-11 15:36:21 +0200

    added seeds

---
+++ seeds/admin.seed.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import * as process from 'process';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const adminEmail = process.env.ADMIN_EMAIL!;
+  const adminPassword = process.env.ADMIN_PASSWORD!;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(adminPassword, salt);
+
+  const admin = await prisma.user.upsert({
+    where: { email: adminEmail },
+    update: {},
+    create: {
+      email: adminEmail,
+      username: 'Admin-User-1',
+      password: hashedPassword,
+      role: 'ADMIN',
+    },
+  });
+
+  console.log({ admin });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
